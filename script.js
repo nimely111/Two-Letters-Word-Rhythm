703b607ed369338acc51f88978ec77e932111390
nimely111 <samuelehnimely@gmail.com>
2024-03-24 21:14:50 +0000

    add click event to all buttons content

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const numberOfSpeechButtons = document.querySelectorAll('.speech').length;
 console.log(numberOfSpeechButtons);
 
 for(let i = 0; i < numberOfSpeechButtons; i++){
-
+    numberOfSpeechButtons[i].addEventListener('click', function(){
+        let buttonContentClick = this.innerHTML;
+        console.log(buttonContentClick);
+    })
 }

60b4bd336071136737a48115e9f47f3ccda42179
nimely111 <samuelehnimely@gmail.com>
2024-03-24 21:18:54 +0000

    called the make sound and button animation function

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ for(let i = 0; i < numberOfSpeechButtons; i++){
     numberOfSpeechButtons[i].addEventListener('click', function(){
         let buttonContentClick = this.innerHTML;
         console.log(buttonContentClick);
+
+        // calling the make sound function
+        makeSound(buttonContentClick);
+        // calling the button animation function
+        buttonAnimation(buttonContentClick);
+        
     })
 }
